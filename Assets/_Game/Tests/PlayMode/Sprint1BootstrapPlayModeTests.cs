91480cabb5cb9cc19bc3b2a014e56fc0087b116f
mbkaya879@gmail.com <mbkaya879@gmail.com>
2026-09-06 20:56:41 +0300

    test: stabilize sprint 1 bootstrap play mode smoke test

--- Assets/_Game/Tests/PlayMode/Sprint1BootstrapPlayModeTests.cs
+++ Assets/_Game/Tests/PlayMode/Sprint1BootstrapPlayModeTests.cs
@@ -8,16 +8,24 @@ namespace TilkiOyunu.Foundation.PlayModeTests
 {
     public sealed class Sprint1BootstrapPlayModeTests
     {
+        [TearDown]
+        public void TearDown()
+        {
+            foreach (GameBootstrap bootstrap in Object.FindObjectsByType<GameBootstrap>(FindObjectsSortMode.None))
+            {
+                Object.DestroyImmediate(bootstrap.gameObject);
+            }
+
+            if (GameServices.HasCurrent)
+            {
+                GameServices.Shutdown();
+            }
+        }
+
         [UnityTest]
         public IEnumerator BootstrapLoadsForestGameplayPrototype()
         {
-            yield return SceneManager.LoadSceneAsync(SceneIds.Bootstrap, LoadSceneMode.Single);
-
-            float timeoutAt = Time.realtimeSinceStartup + 5f;
-            while (SceneManager.GetActiveScene().name != SceneIds.Forest && Time.realtimeSinceStartup < timeoutAt)
-            {
-                yield return null;
-            }
+            yield return LoadBootstrapToForest();
 
             Assert.That(SceneManager.GetActiveScene().name, Is.EqualTo(SceneIds.Forest));
             Assert.That(GameServices.HasCurrent, Is.True);
@@ -28,5 +36,19 @@ namespace TilkiOyunu.Foundation.PlayModeTests
             Assert.That(Object.FindFirstObjectByType<InteractionPromptUI>(), Is.Not.Null);
             Assert.That(Object.FindFirstObjectByType<TestInteractable>(), Is.Not.Null);
         }
+
+        private static IEnumerator LoadBootstrapToForest()
+        {
+            yield return SceneManager.LoadSceneAsync(SceneIds.Bootstrap, LoadSceneMode.Single);
+
+            float timeoutAt = Time.realtimeSinceStartup + 5f;
+            while (SceneManager.GetActiveScene().name != SceneIds.Forest && Time.realtimeSinceStartup < timeoutAt)
+            {
+                yield return null;
+            }
+
+            yield return null;
+        }
+
     }
 }
